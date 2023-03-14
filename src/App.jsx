286773fb0b9bd1fa42b09de59e8b6505e22be975
darkdave1185 <darkdave1185@gmail.com>
2023-03-14 10:47:54 -0700

    update 3rd practice changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import './App.css'
 import CardAnimation from "./components/CardAnimation.jsx"
 import CardVariant1 from "./components/CardVariant1.jsx"
 import CardVariant2 from "./components/CardVariant2.jsx"
+import CardVariant3 from "./components/CardVariant3.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div className='card-section'>
-        <h1 className="card-section-title">Card Practice Base</h1>
-        <h5 className="credits">Youtube:OnlineTutorial (www.youtube.com/@OnlineTutorialsYT/featured)</h5>
+      <div className='base card-section'>
+        <h1 className="base card-section-title">Card Practice Base</h1>
+        <h5 className="base credits">Youtube:OnlineTutorial (www.youtube.com/@OnlineTutorialsYT/featured)</h5>
         <CardAnimation />
       </div>
       <div className='practice-1 card-section'>
@@ -24,6 +25,11 @@ function App() {
         <h1 className="ractice-2 card-section-title">Card Practice 2</h1>
         <h5 className="practice-2 credits">updated hyperlink and hover effect...</h5>
         <CardVariant2 />
+      </div>
+      <div className='practice-3 card-section'>
+        <h1 className="ractice-3 card-section-title">Card Practice 3</h1>
+        <h5 className="practice-3 credits">updated hyperlink ...</h5>
+        <CardVariant3 />
       </div>
     </div>
   )
--- src/components/CardAnimation.jsx
+++ src/components/CardAnimation.jsx
@@ -5,12 +5,12 @@ import "./CardAnimation.css"
 function CardAnimation() {
   return (
     <div className='base'>
-        <div className='body'>
-            <div className='container'>
-                <div className="box style1"></div>
-                <div className="box style2"></div>
-                <div className="box style3"></div>
-                <div className="box style4"></div>
+        <div className='base body'>
+            <div className='base container'>
+                <div className="base box style1"></div>
+                <div className="base box style2"></div>
+                <div className="base box style3"></div>
+                <div className="base box style4"></div>
             </div>
         </div>
     </div>
